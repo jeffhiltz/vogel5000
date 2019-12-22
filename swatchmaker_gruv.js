f795a96391e93a14738e0998934b323b5e70c19c
Jeff Hiltz <jeffhiltz@gmail.com>
2019-12-22 15:48:41 +0100

    basic gruvbox style swatches

--- swatchmaker_gruv.js
+++ swatchmaker_gruv.js
@@ -1,21 +1,26 @@
 const fs = require('fs')
+const yaml = require('js-yaml');
 
-const inputFile = '.Xresources'
-const outputFile = 'swatch.svg'
+const inputFile = 'colours.yaml'
+const outputFile = 'swatch_gruvstyle.svg'
 
-const xresources = fs.readFileSync(inputFile, {encoding: 'utf8'})
-const xcolors = {}
-const regex = /(background|foreground|color[0-9]{1,2}):\s*(#[A-Fa-f0-9]{6})/g
-let result
-while ((result = regex.exec(xresources)) !== null) {
-  xcolors[result[1]] = result[2]
+let colours
+
+// read colours from file
+try {
+  const doc = yaml.safeLoad(fs.readFileSync(inputFile, 'utf8'))
+  colours = doc.vogel5000
+} catch (e) {
+  console.log(e)
 }
 
-const swatchWidth = 348
-const swatchHeight = 240
+
+
+const swatchWidth = 348 / 2
+const swatchHeight = 240 * 0.6
 
 const top = `<?xml version="1.0" encoding="utf-8" ?>
-<svg id="drawing" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="1100" height="200">
+<svg id="drawing" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="${swatchWidth * 8}" height="${swatchHeight * 4}">
 `
 const style = `
   <style>
@@ -28,19 +33,18 @@ const style = `
   </style>
 `
 
-const backgroundRect = `
-  <rect width="1100" height="200" fill="${xcolors.background}"></rect>
-`
-function swatch(name, colorHash, x, y) {
-  const isBackground = colorHash === xcolors.background
+function swatch(name, rowIdx, colIdx) {
+  const x = colIdx * swatchWidth
+  const y = rowIdx * swatchHeight
+  const colorHash = colours[name]
   return `
   <g>
     <rect width="${swatchWidth}" height="${swatchHeight}" fill="${colorHash}" x="${x}" y="${y}"></rect>
-    <text class="name" fill="${isBackground ? xcolors.foreground : colorHash}" x="${x}" y="${y}">
-      <tspan dx="50" dy="-10">${name}</tspan>
+    <text class="name" fill="${colours['grey10']}" x="${x}" y="${y}">
+      <tspan dx="${swatchWidth / 2}" dy="${swatchHeight * 0.33}">${name}</tspan>
     </text>
-    <text class="hash" fill="${isBackground ? xcolors.foreground : xcolors.background}" x="${x}" y="${y}">
-      <tspan dx="50" dy="30">${colorHash}</tspan>
+    <text class="hash" fill="${colours['grey10']}" x="${x}" y="${y}">
+      <tspan dx="${swatchWidth / 2}" dy="${swatchHeight * 0.66}">${colorHash}</tspan>
     </text>
   </g>
 `
@@ -50,33 +54,59 @@ const bottom = `
 </svg>
 `
 
+const darkLayout = [
+  [
+    'grey0',
+    'darkred',
+    'darkgreen',
+    'darkyellow',
+    'darkblue',
+    'darkviolet',
+    'darkcyan',
+    'grey6',
+  ],
+  [
+    'grey5',
+    'red',
+    'green',
+    'yellow',
+    'blue',
+    'violet',
+    'cyan',
+    'grey9',
+  ],
+  [
+    'grey0hard',
+    'grey0',
+    'grey1',
+    'grey2',
+    'grey3',
+    'grey4',
+    'grey5',
+    'darkorange',
+  ],
+  [
+    'grey0',
+    'grey0soft',
+    'grey6',
+    'grey7',
+    'grey8',
+    'grey9',
+    'grey10',
+    'orange',
+  ],
+]
+
 const outFile = fs.createWriteStream(outputFile)
 outFile.on('open', () => {
   outFile.write(top)
   outFile.write(style)
-  outFile.write(backgroundRect)
 
-  xPos = 0
-  yPos = 0
-
-  function drawSwatch(name) {
-    outFile.write(swatch(name, xcolors[name], xPos, yPos))
-    if (xPos >= 960) {
-      xPos = 20
-      yPos += 90
-    } else {
-      xPos += 120
-    }
-  }
-
-  drawSwatch('foreground')
-  for (let i = 0; i < 8; i++) {
-    drawSwatch(`color${i}`)
-  }
-  drawSwatch('background')
-  for (let i = 8; i < 16; i++) {
-    drawSwatch(`color${i}`)
-  }
+  darkLayout.forEach((row, rowIdx) => {
+    row.forEach((label, colIdx) => {
+      outFile.write(swatch(label, rowIdx, colIdx))
+    })
+  })
 
   outFile.write(bottom)
 })
