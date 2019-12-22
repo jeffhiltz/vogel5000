05bd7e7f0d6ad43257f497d20a974661e378cc91
Jeff Hiltz <jeffhiltz@gmail.com>
2019-12-22 16:13:30 +0100

    change layout of colours and add auto foreground selection

--- swatchmaker_gruv.js
+++ swatchmaker_gruv.js
@@ -1,8 +1,9 @@
+const convert = require('color-convert')
 const fs = require('fs')
 const yaml = require('js-yaml');
 
 const inputFile = 'colours.yaml'
-const outputFile = 'swatch_gruvstyle.svg'
+const outputFile = 'swatch_mystyle.svg'
 
 let colours
 
@@ -19,16 +20,23 @@ try {
 const swatchWidth = 348 / 2
 const swatchHeight = 240 * 0.6
 
+const colCount = 8
+const rowCount = 5
+
 const top = `<?xml version="1.0" encoding="utf-8" ?>
-<svg id="drawing" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="${swatchWidth * 8}" height="${swatchHeight * 4}">
+<svg id="drawing" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="${swatchWidth * colCount}" height="${swatchHeight * rowCount}">
 `
 const style = `
   <style>
     .hash {
       text-anchor: middle;
+      font-family: sans-serif;
+      font-size: ${swatchHeight * 0.2}px;
     }
     .name {
       text-anchor: middle;
+      font-family: sans-serif;
+      font-size: ${swatchHeight * 0.2}px;
     }
   </style>
 `
@@ -37,13 +45,14 @@ function swatch(name, rowIdx, colIdx) {
   const x = colIdx * swatchWidth
   const y = rowIdx * swatchHeight
   const colorHash = colours[name]
+  const textColour = convert.hex.lab(colorHash)[0] < 50 ? colours['grey10'] : colours['grey0']
   return `
   <g>
     <rect width="${swatchWidth}" height="${swatchHeight}" fill="${colorHash}" x="${x}" y="${y}"></rect>
-    <text class="name" fill="${colours['grey10']}" x="${x}" y="${y}">
+    <text class="name" fill="${textColour}" x="${x}" y="${y}">
       <tspan dx="${swatchWidth / 2}" dy="${swatchHeight * 0.33}">${name}</tspan>
     </text>
-    <text class="hash" fill="${colours['grey10']}" x="${x}" y="${y}">
+    <text class="hash" fill="${textColour}" x="${x}" y="${y}">
       <tspan dx="${swatchWidth / 2}" dy="${swatchHeight * 0.66}">${colorHash}</tspan>
     </text>
   </g>
@@ -54,46 +63,95 @@ const bottom = `
 </svg>
 `
 
+// const darkLayout = [
+//   [
+//     'grey0',
+//     'darkred',
+//     'darkgreen',
+//     'darkyellow',
+//     'darkblue',
+//     'darkviolet',
+//     'darkcyan',
+//     'grey6',
+//   ],
+//   [
+//     'grey5',
+//     'red',
+//     'green',
+//     'yellow',
+//     'blue',
+//     'violet',
+//     'cyan',
+//     'grey9',
+//   ],
+//   [
+//     'grey0hard',
+//     'grey0',
+//     'grey1',
+//     'grey2',
+//     'grey3',
+//     'grey4',
+//     'grey5',
+//     'darkorange',
+//   ],
+//   [
+//     'grey0',
+//     'grey0soft',
+//     'grey6',
+//     'grey7',
+//     'grey8',
+//     'grey9',
+//     'grey10',
+//     'orange',
+//   ],
+// ]
+
 const darkLayout = [
   [
-    'grey0',
     'darkred',
     'darkgreen',
     'darkyellow',
     'darkblue',
     'darkviolet',
     'darkcyan',
-    'grey6',
+    'darkorange',
   ],
   [
-    'grey5',
     'red',
     'green',
     'yellow',
     'blue',
     'violet',
     'cyan',
-    'grey9',
+    'orange',
+  ],
+  [
+    'brightred',
+    'brightgreen',
+    'brightyellow',
+    'brightblue',
+    'brightviolet',
+    'brightcyan',
+    'brightorange',
   ],
   [
     'grey0hard',
     'grey0',
+    'grey0soft',
     'grey1',
     'grey2',
     'grey3',
     'grey4',
-    'grey5',
-    'darkorange',
   ],
   [
-    'grey0',
-    'grey0soft',
+    'grey5',
     'grey6',
     'grey7',
     'grey8',
     'grey9',
+    'grey10soft',
     'grey10',
-    'orange',
+    'grey10hard',
   ],
 ]
 
